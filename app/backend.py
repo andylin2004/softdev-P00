10bca79ab373270c3889569862b7a3ea1c66f13c
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-01 09:11:29 -0400

    added another func to backend

--- app/backend.py
+++ app/backend.py
@@ -6,3 +6,8 @@ c = db.cursor()
 def search(searchQuery):
     c.execute("SELECT * FROM blogs WHERE blogTitle LIKE '%:title%'", {'title': searchQuery})
     data = c.fetchall()
+
+def pullUserData(userID):
+    c.execute("SELECT * FROM blogs WHERE author IS ':userID'", {'userId': userID})
+    data = c.fetchall()
+

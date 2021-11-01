a3b015a515e509b1b361d9bdc09daa71ea1330d9
xVany <rayy12204@gmail.com>
2021-11-01 19:10:59 -0400

    routing of templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,32 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 from utils.database import initializeDatabase;
 
 @app.route("/")
 def index():
-    return "Hey!"
+    return render_template( 'homePage.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/editBlog")
+def editBlog():
+    return render_template('editBlog.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    return "filler"
 
 if __name__ == "__main__":  # true if this file NOT imported
     initializeDatabase()

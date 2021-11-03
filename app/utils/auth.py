c3c08d9ce02069dfb7494c925229476687665b97
OnikuSpedu <srakibnyc1@gmail.com>
2021-11-03 09:00:14 -0400

    Started working on hashing

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,6 +1,7 @@
 from flask import Flask             #facilitate flask webserving
 from flask import session           #facilitate user sessions
-from utils.db import initializeUsersTable, addUser, getUserByUsername;
+from utils.db import initializeUsersTable, addUser, getUserByUsername
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class AuthService:
 
@@ -13,19 +14,20 @@ class AuthService:
         if "username" in session:
             userData = getUserByUsername(session.get("username"))
             return userData
-        
+
         return None
 
     def login(self, username, password):
         userData = getUserByUsername(username)
 
         if userData and username == userData[1]:
-            if password == userData[3]:
+            if check_password_hash(userData[3], password):
                 session["username"] = username
                 return True
 
         return False
-        
+
 
     def register(self, username, displayName, password):
-        addUser(username, displayName, password)
+        hashedPw = generate_password_hash(password)
+        addUser(username, displayName, hashedPw)
